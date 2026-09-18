7ced84815ebd1d67735cb13399750dae4fb14b2c
NullCode1337 <contact.nullcode@proton.me>
2026-09-19 00:59:49 +0600

    ktm: Disable wireless charging and powershare
    Change-Id: Ib94104a9306a074a3381b935807eff4a053e24ee

--- device.mk
+++ device.mk
@@ -49,10 +49,6 @@ PRODUCT_COPY_FILES += \
 # Power
 $(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
 
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare-service.oplus
-
 # Regional properties
 REGIONAL_PROP_FILES := $(wildcard $(LOCAL_PATH)/properties/*/*.prop)
 
